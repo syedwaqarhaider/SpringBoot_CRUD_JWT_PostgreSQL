772f1e49ca60dcad3e47800f15676694dc7a2915
Moosa <macbook@Bukharis-MacBook-Pro.local>
2022-06-09 17:22:39 +0500

    Project Setup Complete

---
+++ src/test/java/com/student/studentregistrationapi/StudentRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.studentregistrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
